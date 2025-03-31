5871bce03156313bf1788a73d5ac5dacd19f2e79
Bhokwe <165327116+Bhokwe@users.noreply.github.com>
2025-03-31 15:01:05 +0200

    Movement script

--- Endless Runner POE/Assets/Scripts/PlayerMovement.cs
+++ Endless Runner POE/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float playerSpeed = 3;
+    public float horizontalSpeed = 4;
+    
 
     // Update is called once per frame
     void Update()
     {
+        //player speed relative to the game world
+        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
+
+        //Binds movement
+        if (Input.GetKey(KeyCode.A))
         
     }
 }
