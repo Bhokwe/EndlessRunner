75777c542c3035e71c4bcca7c515c13fef3f6f67
Thand04 <105203300+Thand04@users.noreply.github.com>
2025-03-31 15:02:04 +0200

    Update EndLevelCheckpoints.cs

--- Endless Runner POE/Assets/Scripts/EndLevelCheckpoints.cs
+++ Endless Runner POE/Assets/Scripts/EndLevelCheckpoints.cs
@@ -1,16 +1,31 @@
+using TMPro;
 using UnityEngine;
+
 
 public class EndLevelCheckpoints : MonoBehaviour
 {
+    [SerializeField]
+    GameObject UI;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UI.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnEnterTrigger(Collider collider) //Player to interact with checkpoint to complete the level
+    {
+        Debug.Log("Level {Level Counter} Complete!!");//Checkpoint Display
+
+    }
+    //Checkpoint to behave as teleporter/NewLevelStart
+    private class CheckpointLevelTeleport(Collision collision)
+    {
+
     }
 }
