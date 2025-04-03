852704e9616d46b4479dc40d3908489c3bf7769a
Bhokwe <165327116+Bhokwe@users.noreply.github.com>
2025-04-03 18:20:18 +0200

    Score, coins and Game manager all up and running

--- My project/Assets/Scripts/GroundTile.cs
+++ My project/Assets/Scripts/GroundTile.cs
@@ -11,6 +11,7 @@ public class GroundTile : MonoBehaviour
         //    //Debug.LogError("Its not found in the scene");
         //}
         SpawnObstacle();
+        CoinSpawner();
     }
 
     void OnTriggerExit(Collider other)
@@ -40,5 +41,45 @@ public class GroundTile : MonoBehaviour
         // Spawn the obstacle at the position
         Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
     }
+    public GameObject coinPrefab;
 
+    void CoinSpawner()
+    {
+        int coinsSpawned = 8;
+        for (int i = 0; i < coinsSpawned; i++) 
+         {
+            GameObject temp = Instantiate(coinPrefab, transform);
+            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+        
+        
+        
+        }
+
+
+    }
+
+    Vector3 GetRandomPointInCollider(Collider collider)
+    {
+            Vector3 point = new Vector3
+                (
+                Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+                Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+                Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+                );
+            if(point != collider.ClosestPoint(point))
+            {
+              point = GetRandomPointInCollider (collider);
+
+            }
+
+        point.y = 1;
+        return point;
+
+            
+
+
+            
+
+
+    }
 }
